8b863f5155cc3ea0f3cf69733b32396539404306
bmd1214 <154357694+bmd1214@users.noreply.github.com>
2024-02-12 10:09:57 +0300

    Update Validation.php
    some changes in the Validation.php file

--- SupplierOffer/php/Validation.php
+++ SupplierOffer/php/Validation.php
@@ -2,9 +2,9 @@
 
 class Validation{
 
-    public static function validateData($name, $phone, $email, $carType, $year, $price) : bool{
+    public static function validateData($supplierName, $supplierPhone, $supplierEmail, $carType, $year, $price) : bool{
 
-            if (filter_var($email, FILTER_VALIDATE_EMAIL)){ //check the email
+            if (filter_var($supplierEmail, FILTER_VALIDATE_EMAIL)){ //check the supplier$supplierEmail
                 
             }
             else{
@@ -12,22 +12,22 @@ class Validation{
             }
         
         
-            $length = strlen($name);
-            if($length < 8 || $length > 16){ //check the name length
+            $length = strlen($supplierName);
+            if($length < 8 || $length > 16){ //check the supplierName length
                 throw new Exception("The Name should be in the range of 8 to 16 !");
             }
         // validate if the fields are empty
-            if(empty($name)){ 
-                throw new Exception("The name field is empty!");
+            if(empty($supplierName)){ 
+                throw new Exception("The supplierName field is empty!");
             }
-            if(empty($email)){
-                throw new Exception("The email field is empty!");
+            if(empty($supplierEmail)){
+                throw new Exception("The supplier$supplierEmail field is empty!");
             }
             if(empty($carType)){
                 throw new Exception("You should input the car Type");
             }
-            if(empty($phone)){
-                throw new Exception("You should enter the phone number!");
+            if(empty($supplierPhone)){
+                throw new Exception("You should enter the supplierPhone number!");
             }
             if(empty($price)){
                 throw new Exception("You should enter the Price!");
@@ -35,13 +35,13 @@ class Validation{
             if(empty($year)){
                 throw new Exception("You should enter the Address!");
             }
-        // the name should be all characters
-            if(!(ctype_alpha($name))){
+        // the supplierName should be all characters
+            if(!(ctype_alpha($supplierName))){
                 throw new Exception("The Name Can't contain any numbers or A special character!");
         
             }
-        // phone number should be all digits
-            if(!is_numeric($phone)){
+        // supplierPhone number should be all digits
+            if(!is_numeric($supplierPhone)){
                 throw new Exception("The Phone Number should Be all Digits!");
             }
 
