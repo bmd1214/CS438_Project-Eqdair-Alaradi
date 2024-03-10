5900406640684fb04700e23c8d5ef4f21518c045
bmd1214 <154357694+bmd1214@users.noreply.github.com>
2024-03-10 20:31:59 +0300

    some editing

--- ImportedCars/php/Main.php
+++ ImportedCars/php/Main.php
@@ -1,53 +1,56 @@
 <?php
 
-// this is the main program where objects have been created,
 
 require_once 'Sedan.php';
 require_once 'SedanFactory.php';
 require_once 'ManageCar.php';
 require_once 'Validation.php';
 
-// check if the submit button been pressed
 
-if(isset($_POST['submit'])){
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
 
-    // sign the inserted data into variables
-    $factoryName = 'Sedan' . 'Factory';
-    $type = $_POST['type'];
-    $makeYear = $_POST['year'];
-    $fuelType = $_POST['fuelType'];
-    $mileage = $_POST['mileage'];
-    $vin = $_POST['vin'];
-    $carPrice = $_POST['price'];
-    $color = $_POST['color'];
-    $transmissionType = $_POST['transmissionType'];
-    $quantity = $_POST['Quantity'];
+    // check if the submit button been pressed
+    if(isset($_POST['submit'])){
 
-
-}else{
-    echo "There is somthing wrong with the submission.";
-    exit;
-}
+        // sign the inserted data into variables
+        $factoryName = 'Sedan' ; // 'Sedan' since this is the only type that the company imports
+        $type = $_POST['type'];
+        $makeYear = $_POST['year'];
+        $fuelType = $_POST['fuelType'];
+        $mileage = $_POST['mileage'];
+        $vin = $_POST['vin'];
+        $carPrice = $_POST['price'];
+        $color = $_POST['color'];
+        $transmissionType = $_POST['transmissionType'];
+        $quantity = $_POST['Quantity'];
 
 
-try{
-    if(!class_exists($factoryName)){
-        throw new Exception("The Vehicle type does not Exist!");
+    }else{
+        echo "There is somthing wrong with the submission.";
+        exit;
     }
-    
 
-    $factory = new $factoryName();
-    $sedan = $factory::createVehicle($type, $makeYear, $carPrice, $fuelType, $mileage, $vin, $color, $transmissionType, $quantity);
-    
-    new ManageCar($sedan);
+    try{
+        // adding the word 'Factory' to specify which class will be called
+        $factoryName .= 'Factory';
 
-}catch(Exception $e){
+        if(!class_exists($factoryName)){
+            throw new Exception("The Vehicle type does not Exist!");
+        }
+        
+        // 
+        $sedan = $factoryName::createVehicle($type, $makeYear, $carPrice, $fuelType, $mileage, $vin, $color, $transmissionType, $quantity);
+        
+        new ManageCar($sedan);
 
-    echo $e->getMessage();
-    exit;  // here will exit the program if the user entered data that are not allowed.
+    }catch(Exception $e){
+
+        echo $e->getMessage();
+        exit;  // here will exit the program if the user entered data that are not allowed.
+    }
+
+    // stay in the same page after succeful submission
+    header("refresh:2;url=../html/importedCars.html");
 }
-
-// stay in the same page after succeful submission
-header("refresh:2;url=../html/importedCars.html");
  
 ?>
